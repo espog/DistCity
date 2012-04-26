a6eb6862cf90ed733e81d89d9b2b5054f4ffbb60
Martin Meys <martin.meys@gmail.com>
2012-04-26 16:06:24 +0200

    Creating a BankNode package

--- src/algorithms/BestEffortBroadcast.java
+++ src/algorithms/BestEffortBroadcast.java
@@ -1,9 +1,18 @@
 package algorithms;
+import channels.Message;
 
 public class BestEffortBroadcast {
 
 	public BestEffortBroadcast() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void Broadcast(Message m){
+		
+	}
+	
+	public void Deliver(Message m){
+		
 	}
 
 }
---
+++ src/bankNode/BankNode.java
@@ -0,0 +1,13 @@
+package bankNode;
+
+import java.util.HashMap;
+
+public class BankNode {
+	
+	private HashMap BankMap;
+
+	public BankNode() {
+		BankMap = new HashMap(10);
+	}
+
+}
